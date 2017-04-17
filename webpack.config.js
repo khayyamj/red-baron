ed544875fc362a3dea02053a6442c4a2b260798c
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-04-17 12:01:06 -0600

    setting up webpack2, step 2: custom HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path')
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Red Baron',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    })
+  ]
 }
