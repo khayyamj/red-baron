28a9774f336a42ace0449deb59a550f4e34af53b
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-02 19:34:02 -0600

    player consts loading before player, throwing errors

--- src/Video_Player.js
+++ src/Video_Player.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 export default class VideoPlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.togglePlay = this.togglePlay.bind(this);
+  }
   render() {
+
     return (
       <div className="video-player-wrapper">
-        <video className="video-player__video viewer" src="https://player.vimeo.com/external/194837908.sd.mp4?s=c350076905b78c67f74d7ee39fdb4fef01d12420&profile_id=164">
+        <video className="video-player__video viewer" src="https://player.vimeo.com/external/194837908.sd.mp4?s=c350076905b78c67f74d7ee39fdb4fef01d12420&profile_id=164 autoplay"
+          onClick={this.togglePlay(video)}>
         </video>
 
         <div className="video-player__controls">
           <div className="video-progress">
             <div className="video-progress__filled"></div>
           </div>
-          <button className="video-player__button toggle" title="Toggle Play">►</button>
+          <button className="video-player__button video-toggle" title="Toggle Play">►</button>
           <input type="range" name="volume" className="video-player__slider" min="0" max="1" step="0.05" value="1" />
           <input type="range" name="playbackRate" className="video-player__slider" min="0.5" max="2" step="0.1" value="1" />
           <button data-skip="-10" className="video-player__button">« 10s</button>
@@ -19,5 +25,35 @@ export default class VideoPlayer extends Component {
         </div>
       </div>
     )
+  } // end render
+
+  componentDidMount() {
+    player = document.querySelector('.video-player-wrapper');
+    console.log('player ', player);
+    video = player.querySelector('.viewer');
+    progress = player.querySelector('.video-progress');
+    progressBar = player.querySelector('.video-progress__filled');
+    toggle = player.querySelector('.video-toggle');
+    skipButtons = player.querySelectorAll('[data-skip]');
+    ranges = player.querySelectorAll('.video-player__slider');
+
   }
+
+
+  // build out functions
+  togglePlay(video) {
+    console.log('togglePlay function called ');
+    video.paused ? video.play() : video.pause;
+  }
+
+  // hook up the event listeners
+
 }
+// get our elements
+let player;
+let video = null;
+let progress;
+let progressBar;
+let toggle;
+let skipButtons;
+let ranges;
